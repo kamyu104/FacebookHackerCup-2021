8f65fc5a9c9856c3d81020e36b86e98e3f501651
Allen Liu <kamyu104@gmail.com>
2021-09-13 03:10:42 +0800

    update

--- Round 1/weak_typing_chapter_3.py
+++ Round 1/weak_typing_chapter_3.py
@@ -13,11 +13,14 @@ def matrix_mult(A, B):
     ZB = zip(*B)
     return [[sum(a*b % MOD for a, b in izip(row, col)) % MOD for col in ZB] for row in A]
 
+def identity_matrix(N):
+    return [[int(i == j) for j in xrange(N)] for i in xrange(N)]
+
 def solution():
     N = input()
     W = raw_input().strip()
 
-    result = [[int(i == j) for j in xrange(6)] for i in xrange(6)]
+    result = identity_matrix(6)
     for c in W:
         result = matrix_mult(result, T[c] if c != '.' else result)
     return matrix_mult([[0, 0, 0, 0, 0, 1]], result)[0][0]
@@ -43,7 +46,6 @@ T = {
            [0, 0, 0, 0, 1, 0],
            [0, 0, 0, 0, 1, 1]]
     }
-
 MOD = 10**9+7
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, solution())
