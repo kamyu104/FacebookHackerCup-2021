9bf8395d1ce9268ee9b5babae6af5dd1fafb3022
Allen Liu <kamyu104@gmail.com>
2021-09-13 16:43:35 +0800

    update

--- Round 1/weak_typing_chapter_3-2.py
+++ Round 1/weak_typing_chapter_3-2.py
@@ -21,13 +21,13 @@ def mulmod(a, b):
 def merge(A, B):
     C = [addmod(addmod(addmod(A[RES], B[RES]), mulmod(A[LEFT_SUM], B[LEN])), mulmod(A[LEN], B[RIGHT_SUM])),
          addmod(A[LEN], B[LEN]),
-         addmod(A[SWITCH], B[SWITCH]),
-         addmod(addmod(A[LEFT_SUM], B[LEFT_SUM]), mulmod(A[LEN], B[SWITCH])),
-         addmod(addmod(A[RIGHT_SUM], B[RIGHT_SUM]), mulmod(A[SWITCH], B[LEN])),
+         addmod(A[ACCU], B[ACCU]),
+         addmod(addmod(A[LEFT_SUM], B[LEFT_SUM]), mulmod(A[LEN], B[ACCU])),
+         addmod(addmod(A[RIGHT_SUM], B[RIGHT_SUM]), mulmod(A[ACCU], B[LEN])),
          [addmod(A[LEN], B[FIRST][0]), B[FIRST][1]] if A[FIRST][0] == -1 and B[FIRST][0] != -1 else A[FIRST][:],
          [addmod(A[LEN], B[LAST][0]), B[LAST][1]] if B[LAST][0] != -1 else A[LAST][:]]
     if A[LAST][0] != -1 and B[FIRST][0] != -1 and A[LAST][1] != B[FIRST][1]:
-        C[SWITCH] = addmod(C[SWITCH], 1)
+        C[ACCU] = addmod(C[ACCU], 1)
         left, right = addmod(A[LAST][0], 1), submod(B[LEN], B[FIRST][0])
         C[LEFT_SUM] = addmod(C[LEFT_SUM], left)
         C[RIGHT_SUM] = addmod(C[RIGHT_SUM], right)
@@ -49,6 +49,6 @@ def weak_typing_chapter_3():
     return result[RES]
 
 MOD = 10**9+7
-RES, LEN, SWITCH, LEFT_SUM, RIGHT_SUM, FIRST, LAST = range(7)
+RES, LEN, ACCU, LEFT_SUM, RIGHT_SUM, FIRST, LAST = range(7)
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, weak_typing_chapter_3())
