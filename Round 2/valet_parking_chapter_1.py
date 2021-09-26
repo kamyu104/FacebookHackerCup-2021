b031da292eee236a8ce694c381b569d2f1b7f2ec
Allen Liu <kamyu104@gmail.com>
2021-09-27 07:00:51 +0800

    update

--- Round 2/valet_parking_chapter_1.py
+++ Round 2/valet_parking_chapter_1.py
@@ -14,15 +14,16 @@ def valet_parking_chapter_1():
     G = [raw_input().strip() for _ in xrange(R)]
 
     cnts = Counter()
+    left, right = max(K-(min(R, C)-1), 0), min((K+(min(R, C)-1)), R+1)
     for j in xrange(C):
         total = sum(G[i][j] == 'X' for i in xrange(R))
-        curr = sum(1 <= i <= R and G[i-1][j] == 'X' for i in xrange(max(K-(min(R, C)-1), 0)))
-        for i in xrange(max(K-(min(R, C)-1), 0), min((K+(min(R, C)-1)), R+1)+1):
+        curr = sum(1 <= i <= R and G[i-1][j] == 'X' for i in xrange(left))
+        for i in xrange(left, right+1):
             has_x = (1 <= i <= R and G[i-1][j] == 'X')
             curr += has_x
             if has_x or curr >= K or total-curr >= R-K+1:
                 cnts[i] += 1
-    return min(abs(i-K)+cnts[i] for i in xrange(max(K-(min(R, C)-1), 0), min((K+(min(R, C)-1)), R+1)+1))
+    return min(abs(i-K)+cnts[i] for i in xrange(left, right+1))
 
 for case in xrange(input()):
     print 'Case #%d: %s' % (case+1, valet_parking_chapter_1())
