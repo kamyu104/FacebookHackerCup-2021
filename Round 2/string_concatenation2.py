1e266b4edd076a486c3231be431e9de3414a64a0
Allen Liu <kamyu104@gmail.com>
2021-09-29 04:14:48 +0800

    update

--- Round 2/string_concatenation2.py
+++ Round 2/string_concatenation2.py
@@ -8,42 +8,240 @@
 #
 
 # Template:
-# https://github.com/kamyu104/LeetCode-Solutions/blob/master/Python/range-sum-query-mutable.py
-class BIT(object):  # 0-indexed.
-    def __init__(self, nums):
-        self.__bit = [0]*(len(nums)+1)  # Extra one for dummy node.
-        for i in xrange(1, len(self.__bit)):
-            self.__bit[i] = nums[i-1] + self.__bit[i-1]
-        for i in reversed(xrange(1, len(self.__bit))):
-            last_i = i - (i & -i)
-            self.__bit[i] -= self.__bit[last_i]
+# https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/SortedList.py
+class SortedList(object):
+    def __init__(self, iterable=[], _load=200):
+        """Initialize sorted list instance."""
+        values = sorted(iterable)
+        self._len = _len = len(values)
+        self._load = _load
+        self._lists = _lists = [values[i:i + _load] for i in xrange(0, _len, _load)]
+        self._list_lens = [len(_list) for _list in _lists]
+        self._mins = [_list[0] for _list in _lists]
+        self._fen_tree = []
+        self._rebuild = True
 
-    def add(self, i, val):
-        i += 1  # Extra one for dummy node.
-        while i < len(self.__bit):
-            self.__bit[i] += val
-            i += (i & -i)
+    def _fen_build(self):
+        """Build a fenwick tree instance."""
+        self._fen_tree[:] = self._list_lens
+        _fen_tree = self._fen_tree
+        for i in xrange(len(_fen_tree)):
+            if i | i + 1 < len(_fen_tree):
+                _fen_tree[i | i + 1] += _fen_tree[i]
+        self._rebuild = False
 
-    def query(self, i):
-        i += 1  # Extra one for dummy node.
-        ret = 0
-        while i > 0:
-            ret += self.__bit[i]
-            i -= (i & -i)
-        return ret
+    def _fen_update(self, index, value):
+        """Update `fen_tree[index] += value`."""
+        if not self._rebuild:
+            _fen_tree = self._fen_tree
+            while index < len(_fen_tree):
+                _fen_tree[index] += value
+                index |= index + 1
 
-    # Template:
-    # https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20F/festival.py
-    def kth_element(self, k):
-        floor_log2_n = (len(self.__bit)-1).bit_length()-1
-        pow_i = 2**floor_log2_n
-        total = pos = 0  # 1-indexed
-        for _ in reversed(xrange(floor_log2_n+1)):  # O(logN)
-            if pos+pow_i < len(self.__bit) and total+self.__bit[pos+pow_i] < k:  # find max pos s.t. total < k
-                total += self.__bit[pos+pow_i]
-                pos += pow_i
-            pow_i >>= 1
-        return (pos+1)-1  # 0-indexed, return min pos s.t. total >= k if pos exists else n
+    def _fen_query(self, end):
+        """Return `sum(_fen_tree[:end])`."""
+        if self._rebuild:
+            self._fen_build()
+
+        _fen_tree = self._fen_tree
+        x = 0
+        while end:
+            x += _fen_tree[end - 1]
+            end &= end - 1
+        return x
+
+    def _fen_findkth(self, k):
+        """Return a pair of (the largest `idx` such that `sum(_fen_tree[:idx]) <= k`, `k - sum(_fen_tree[:idx])`)."""
+        _list_lens = self._list_lens
+        if k < _list_lens[0]:
+            return 0, k
+        if k >= self._len - _list_lens[-1]:
+            return len(_list_lens) - 1, k + _list_lens[-1] - self._len
+        if self._rebuild:
+            self._fen_build()
+
+        _fen_tree = self._fen_tree
+        idx = -1
+        for d in reversed(xrange(len(_fen_tree).bit_length())):
+            right_idx = idx + (1 << d)
+            if right_idx < len(_fen_tree) and k >= _fen_tree[right_idx]:
+                idx = right_idx
+                k -= _fen_tree[idx]
+        return idx + 1, k
+
+    def _delete(self, pos, idx):
+        """Delete value at the given `(pos, idx)`."""
+        _lists = self._lists
+        _mins = self._mins
+        _list_lens = self._list_lens
+
+        self._len -= 1
+        self._fen_update(pos, -1)
+        del _lists[pos][idx]
+        _list_lens[pos] -= 1
+
+        if _list_lens[pos]:
+            _mins[pos] = _lists[pos][0]
+        else:
+            del _lists[pos]
+            del _list_lens[pos]
+            del _mins[pos]
+            self._rebuild = True
+
+    def _loc_left(self, value):
+        """Return an index pair that corresponds to the first position of `value` in the sorted list."""
+        if not self._len:
+            return 0, 0
+
+        _lists = self._lists
+        _mins = self._mins
+
+        lo, pos = -1, len(_lists) - 1
+        while lo + 1 < pos:
+            mi = (lo + pos) >> 1
+            if value <= _mins[mi]:
+                pos = mi
+            else:
+                lo = mi
+
+        if pos and value <= _lists[pos - 1][-1]:
+            pos -= 1
+
+        _list = _lists[pos]
+        lo, idx = -1, len(_list)
+        while lo + 1 < idx:
+            mi = (lo + idx) >> 1
+            if value <= _list[mi]:
+                idx = mi
+            else:
+                lo = mi
+
+        return pos, idx
+
+    def _loc_right(self, value):
+        """Return an index pair that corresponds to the last position of `value` in the sorted list."""
+        if not self._len:
+            return 0, 0
+
+        _lists = self._lists
+        _mins = self._mins
+
+        pos, hi = 0, len(_lists)
+        while pos + 1 < hi:
+            mi = (pos + hi) >> 1
+            if value < _mins[mi]:
+                hi = mi
+            else:
+                pos = mi
+
+        _list = _lists[pos]
+        lo, idx = -1, len(_list)
+        while lo + 1 < idx:
+            mi = (lo + idx) >> 1
+            if value < _list[mi]:
+                idx = mi
+            else:
+                lo = mi
+
+        return pos, idx
+
+    def add(self, value):
+        """Add `value` to sorted list."""
+        _load = self._load
+        _lists = self._lists
+        _mins = self._mins
+        _list_lens = self._list_lens
+
+        self._len += 1
+        if _lists:
+            pos, idx = self._loc_right(value)
+            self._fen_update(pos, 1)
+            _list = _lists[pos]
+            _list.insert(idx, value)
+            _list_lens[pos] += 1
+            _mins[pos] = _list[0]
+            if _load + _load < len(_list):
+                _lists.insert(pos + 1, _list[_load:])
+                _list_lens.insert(pos + 1, len(_list) - _load)
+                _mins.insert(pos + 1, _list[_load])
+                _list_lens[pos] = _load
+                del _list[_load:]
+                self._rebuild = True
+        else:
+            _lists.append([value])
+            _mins.append(value)
+            _list_lens.append(1)
+            self._rebuild = True
+
+    def discard(self, value):
+        """Remove `value` from sorted list if it is a member."""
+        _lists = self._lists
+        if _lists:
+            pos, idx = self._loc_right(value)
+            if idx and _lists[pos][idx - 1] == value:
+                self._delete(pos, idx - 1)
+
+    def remove(self, value):
+        """Remove `value` from sorted list; `value` must be a member."""
+        _len = self._len
+        self.discard(value)
+        if _len == self._len:
+            raise ValueError('{0!r} not in list'.format(value))
+
+    def pop(self, index=-1):
+        """Remove and return value at `index` in sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        value = self._lists[pos][idx]
+        self._delete(pos, idx)
+        return value
+
+    def bisect_left(self, value):
+        """Return the first index to insert `value` in the sorted list."""
+        pos, idx = self._loc_left(value)
+        return self._fen_query(pos) + idx
+
+    def bisect_right(self, value):
+        """Return the last index to insert `value` in the sorted list."""
+        pos, idx = self._loc_right(value)
+        return self._fen_query(pos) + idx
+
+    def count(self, value):
+        """Return number of occurrences of `value` in the sorted list."""
+        return self.bisect_right(value) - self.bisect_left(value)
+
+    def __len__(self):
+        """Return the size of the sorted list."""
+        return self._len
+
+    def __getitem__(self, index):
+        """Lookup value at `index` in sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        return self._lists[pos][idx]
+
+    def __delitem__(self, index):
+        """Remove value at `index` from sorted list."""
+        pos, idx = self._fen_findkth(self._len + index if index < 0 else index)
+        self._delete(pos, idx)
+
+    def __contains__(self, value):
+        """Return true if `value` is an element of the sorted list."""
+        _lists = self._lists
+        if _lists:
+            pos, idx = self._loc_left(value)
+            return idx < len(_lists[pos]) and _lists[pos][idx] == value
+        return False
+
+    def __iter__(self):
+        """Return an iterator over the sorted list."""
+        return (value for _list in self._lists for value in _list)
+
+    def __reversed__(self):
+        """Return a reverse iterator over the sorted list."""
+        return (value for _list in reversed(self._lists) for value in reversed(_list))
+
+    def __repr__(self):
+        """Return string representation of sorted list."""
+        return 'SortedList({0})'.format(list(self))
 
 def update_remains(A, B, R):
     R[:] = [i for i in R if i not in A and i not in B]
@@ -62,39 +260,45 @@ def add_equal_1sums(L, A, B, R):  # Time: O(N), N1 = O(N)
     return R
 
 def add_equal_2sums(L, A, B, R):  # Time: O(N1) + O((2 * L) * (1/N1 + 1/(N1-1) + ... + 1/1)) * O(logN1) = O(N1 + L * (logN1)^2) ~= O(6e7), N2 = O(894)
-    cnt = None
-    while len(R) != cnt:  # O(c) times, c ~= 4 at most, first run removes most equal sum pairs, run once is actually enough
-        cnt = len(R)
-        lookup = {}
-        R_inv = {x:i for i, x in enumerate(R)}
-        bit = BIT([1]*len(R))
-        l = 1
-        while l < bit.query(len(R)-1):
-            i = 0
-            while i < bit.query(len(R)-1):
-                if l == bit.query(len(R)-1):
-                    break
-                j = (i+l)%bit.query(len(R)-1)
-                a, b = R[bit.kth_element(i+1)], R[bit.kth_element(j+1)]
-                total = L[a]+L[b]
-                if total in lookup:
-                    for x in lookup[total]:
-                        if x in (a, b) or x in A or x in B:
-                            del lookup[total]
-                            break
-                if total not in lookup:
-                    lookup[total] = (a, b)
-                    i += 1
-                    continue
-                A.add(a), A.add(b)
-                c, d = lookup[total]
-                B.add(c), B.add(d)
-                del lookup[total]
-                for x in (a, b, c, d):
-                    bit.add(R_inv[x], -1)
-                i += 1
-            l += 1
-        update_remains(A, B, R)
+    lookup = {}
+    R_inv = {x:i for i, x in enumerate(R)}
+    sl = SortedList()
+    nxt = {}
+    for i in xrange(len(R)):
+        sl.add(i)
+        nxt[i] = i+1
+    sl.add(len(R))
+    done = False
+    while not done and len(sl) != 1:
+        i = next(iter(sl))
+        while i != len(R):
+            a = R[i]
+            j = sl[sl.bisect_left(nxt[i])]
+            if j == len(R):
+                if i == next(iter(sl)):
+                    done = True
+                break
+            b = R[j]
+            total = L[a]+L[b]
+            if total in lookup:
+                for x in lookup[total]:
+                    assert(x not in (a, b))
+                    if x in A or x in B:
+                        del lookup[total]
+                        break
+            if total not in lookup:
+                lookup[total] = (a, b)
+                nxt[i] = sl[sl.bisect_right(j)]
+                i = sl[sl.bisect_right(i)]
+                continue
+            A.add(a), A.add(b)
+            c, d = lookup[total]
+            B.add(c), B.add(d)
+            del lookup[total]
+            for x in (a, b, c, d):
+                sl.remove(R_inv[x])
+            i = sl[sl.bisect_right(i)]
+    update_remains(A, B, R)
     assert(len(R) <= 894)  # max v s.t. v(v-1)/2! <= 2*MAX_L
     return R
 
