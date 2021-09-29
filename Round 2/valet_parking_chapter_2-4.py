9118589351440b0ae3b3ca6a06560aadbad455d7
Allen Liu <kamyu104@gmail.com>
2021-09-30 07:12:18 +0800

    update

--- Round 2/valet_parking_chapter_2-4.py
+++ Round 2/valet_parking_chapter_2-4.py
@@ -3,11 +3,11 @@
 # Facebook Hacker Cup 2021 Round 2 - Problem C. Valet Parking - Chapter 2
 # https://www.facebook.com/codingcompetitions/hacker-cup/2021/round-2/problems/C2
 #
-# Time:  O((R * C + S) * logR + SlogS), pass in PyPy2 but Python2
+# Time:  O((R * C + S) * logR), pass in PyPy2 but Python2
 # Space: O(R * C + S)
 #
 
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 from collections import defaultdict
 
 # Template:
@@ -93,33 +93,52 @@ class SegmentTree(object):  # 0-based index
             showList.append(self.query(i, i))
         return ",".join(map(str, showList))
 
+def rebuild_heap(heap, to_remove, sign):  # Time: O(R), Space: O(R)
+    result = []
+    for x in heap:
+        if sign*x not in to_remove:
+            result.append(x)
+            continue
+        to_remove[sign*x] -= 1
+        if not to_remove[sign*x]:
+            del to_remove[sign*x]
+    heap[:] = result
+    heapify(heap)
+
 # Template:
 # https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20F/festival4.py
-def update_heaps(K, c, r, heaps):  # Time: O(SlogS), Space: O(S) due to lazy delete
+def update_heaps(K, c, r, heaps):
     topk, others, to_remove = heaps
     if c == 1:
         heappush(topk[0], r)
         topk[1] += 1
         if topk[1] == K+1:  # keep topk with k elements
-            heappush(others, -heappop(topk[0]))
+            heappush(others[0], -heappop(topk[0]))
             topk[1] -= 1
+            others[1] += 1
     else:
         to_remove[r] += 1
-        if not others or -others[0] < r:
+        if others[0] and -others[0][0] >= r:
+            others[1] -= 1
+        else:
             topk[1] -= 1
-            if others:
-                heappush(topk[0], -heappop(others))  # keep topk with k elements
+            if others[0]:
+                heappush(topk[0], -heappop(others[0]))  # keep topk with k elements
+                others[1] -= 1
                 topk[1] += 1
-    while others and -others[0] in to_remove:
-        to_remove[-others[0]] -= 1
-        if not to_remove[-others[0]]:
-            del to_remove[-others[0]]
-        heappop(others)
+    while others[0] and -others[0][0] in to_remove:
+        to_remove[-others[0][0]] -= 1
+        if not to_remove[-others[0][0]]:
+            del to_remove[-others[0][0]]
+        heappop(others[0])
     while topk[0] and topk[0][0] in to_remove:
         to_remove[topk[0][0]] -= 1
         if not to_remove[topk[0][0]]:
             del to_remove[topk[0][0]]
         heappop(topk[0])
+    if len(topk[0])+len(others[0]) > 2*(topk[1]+others[1]):
+        rebuild_heap(others[0], to_remove, -1)
+        rebuild_heap(topk[0], to_remove, 1)
 
 def update(R, K, r, heaps, st, diff):
     h1, h2 = heaps[0][0], heaps[1][0]
@@ -142,7 +161,7 @@ def valet_parking_chapter_2():
     R, C, K, S = map(int, raw_input().strip().split())
     G = [list(raw_input().strip()) for _ in xrange(R)]
 
-    heaps = [[[[[], 0], [], defaultdict(int)] for _ in xrange(2)] for _ in xrange(C)]
+    heaps = [[[[[], 0], [[], 0], defaultdict(int)] for _ in xrange(2)] for _ in xrange(C)]
     st = SegmentTree(R+2, build_fn=lambda x, y: [abs((i-x)-K) if i >= x else y for i in xrange(2*x)])
     for j in xrange(C):
         for i in xrange(R):
--- Round 2/valet_parking_chapter_2-8.py
+++ Round 2/valet_parking_chapter_2-8.py
@@ -3,40 +3,59 @@
 # Facebook Hacker Cup 2021 Round 2 - Problem C. Valet Parking - Chapter 2
 # https://www.facebook.com/codingcompetitions/hacker-cup/2021/round-2/problems/C2
 #
-# Time:  O(S * min(R, C) + (R * C + S) * logR + SlogS)
+# Time:  O(S * min(R, C) + (R * C + S) * logR)
 # Space: O(R * C + S)
 #
 
-from heapq import heappush, heappop
+from heapq import heappush, heappop, heapify
 from collections import defaultdict
+
+def rebuild_heap(heap, to_remove, sign):  # Time: O(R), Space: O(R)
+    result = []
+    for x in heap:
+        if sign*x not in to_remove:
+            result.append(x)
+            continue
+        to_remove[sign*x] -= 1
+        if not to_remove[sign*x]:
+            del to_remove[sign*x]
+    heap[:] = result
+    heapify(heap)
 
 # Template:
 # https://github.com/kamyu104/GoogleKickStart-2021/blob/main/Round%20F/festival4.py
-def update_heaps(K, c, r, heaps):  # Time: O(SlogS), Space: O(S) due to lazy delete
+def update_heaps(K, c, r, heaps):
     topk, others, to_remove = heaps
     if c == 1:
         heappush(topk[0], r)
         topk[1] += 1
         if topk[1] == K+1:  # keep topk with k elements
-            heappush(others, -heappop(topk[0]))
+            heappush(others[0], -heappop(topk[0]))
             topk[1] -= 1
+            others[1] += 1
     else:
         to_remove[r] += 1
-        if not others or -others[0] < r:
+        if others[0] and -others[0][0] >= r:
+            others[1] -= 1
+        else:
             topk[1] -= 1
-            if others:
-                heappush(topk[0], -heappop(others))  # keep topk with k elements
+            if others[0]:
+                heappush(topk[0], -heappop(others[0]))  # keep topk with k elements
+                others[1] -= 1
                 topk[1] += 1
-    while others and -others[0] in to_remove:
-        to_remove[-others[0]] -= 1
-        if not to_remove[-others[0]]:
-            del to_remove[-others[0]]
-        heappop(others)
+    while others[0] and -others[0][0] in to_remove:
+        to_remove[-others[0][0]] -= 1
+        if not to_remove[-others[0][0]]:
+            del to_remove[-others[0][0]]
+        heappop(others[0])
     while topk[0] and topk[0][0] in to_remove:
         to_remove[topk[0][0]] -= 1
         if not to_remove[topk[0][0]]:
             del to_remove[topk[0][0]]
         heappop(topk[0])
+    if len(topk[0])+len(others[0]) > 2*(topk[1]+others[1]):
+        rebuild_heap(others[0], to_remove, -1)
+        rebuild_heap(topk[0], to_remove, 1)
 
 def update(R, K, r, heaps, cnts, left, right, diff):
     h1, h2 = heaps[0][0], heaps[1][0]
@@ -64,7 +83,7 @@ def valet_parking_chapter_2():
     R, C, K, S = map(int, raw_input().strip().split())
     G = [list(raw_input().strip()) for _ in xrange(R)]
 
-    heaps = [[[[[], 0], [], defaultdict(int)] for _ in xrange(2)] for _ in xrange(C)]
+    heaps = [[[[[], 0], [[], 0], defaultdict(int)] for _ in xrange(2)] for _ in xrange(C)]
     left, right = max(K-(C-1), 0), min(K+(C-1), R+1)
     cnts = [abs(i-K) for i in xrange(left, right+1)]
     for j in xrange(C):
